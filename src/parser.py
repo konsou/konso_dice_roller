a5d7db192d67304c453050c492e1da9213c63d30
Tomi Javanainen <tomi.javanainen@gmail.com>
2022-10-27 16:31:52 +0300

    Rename "constant" -> "bonus" in parser.py

--- src/parser.py
+++ src/parser.py
@@ -7,27 +7,27 @@ from src.roll import Roll
 
 def parse_input(input_string: str) -> Roll:
     input_string = input_string.lower().strip()
-    dice, constant = _parse_constant(input_string)
+    dice, bonus = _parse_bonus(input_string)
     number_of_dice, dice_sides = dice.split("d")
     return Roll(
         number_of_dice=int(number_of_dice),
         dice_sides=int(dice_sides),
-        bonus=constant,
+        bonus=bonus,
     )
 
 
-def _parse_constant(input_string: str) -> tuple[str, float]:
-    """Return (dice part, constant)
+def _parse_bonus(input_string: str) -> tuple[str, float]:
+    """Return (dice part, bonus)
     e.g. ("5d6", 2), ("3d10", -3)"""
-    constant_sign = _constant_sign(input_string)
-    if not constant_sign:
+    sign = _sign_in_string(input_string)
+    if not sign:
         return input_string, 0
 
     split = re.split("[+-]", input_string)
-    return split[0], float(f"{constant_sign}{split[1]}")
+    return split[0], float(f"{sign}{split[1]}")
 
 
-def _constant_sign(input_string: str) -> Literal["+", "-", ""]:
+def _sign_in_string(input_string: str) -> Literal["+", "-", ""]:
     if "+" in input_string:
         return "+"
     if "-" in input_string:
