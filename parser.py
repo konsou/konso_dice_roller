5570143d4d5a471d2296e796dec50fadea1ecf74
Tomi Javanainen <tomi.javanainen@gmail.com>
2022-10-27 13:12:50 +0300

    Add support for parsing plus

--- parser.py
+++ parser.py
@@ -2,7 +2,18 @@ from roll import Roll
 
 
 def parse_input(input_string: str) -> Roll:
-    number_of_dice, dice_sides = input_string.split("d")
-    return Roll(number_of_dice=int(number_of_dice),
-                dice_sides=int(dice_sides),
-                bonus=0)
+    dice, constant = _parse_constant(input_string)
+    number_of_dice, dice_sides = dice.split("d")
+    return Roll(
+        number_of_dice=int(number_of_dice),
+        dice_sides=int(dice_sides),
+        bonus=float(constant),
+    )
+
+
+def _parse_constant(input_string: str) -> tuple[str, str]:
+    split = input_string.split("+")
+    try:
+        return split[0], split[1]
+    except IndexError:
+        return split[0], "0"
