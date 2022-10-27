a431973c061207187b70d61c6907aaab9dd6f46c
Tomi Javanainen <tomi.javanainen@gmail.com>
2022-10-27 20:44:56 +0300

    Refactoring

--- src/format.py
+++ src/format.py
@@ -5,13 +5,7 @@ from src.roll import Roll
 
 def roll_as_text(roll: Roll) -> str:
     bonus = getattr(roll, "bonus", 0)
-    if bonus:
-        bonus = as_float_if_has_decimals(bonus)
-        request_bonus = number_as_string_with_sign(bonus)
-        after_rolls_bonus = number_as_string_with_spaces_around_sign(bonus)
-    else:
-        request_bonus = ""
-        after_rolls_bonus = ""
+    request_bonus, after_rolls_bonus = _bonus_strings_from_bonus(bonus)
 
     individual_results_text = " ".join((str(r) for r in roll.individual_results))
 
@@ -20,6 +14,27 @@ def roll_as_text(roll: Roll) -> str:
         f"Rolls: [{individual_results_text}]{after_rolls_bonus} "
         f"Result: {as_float_if_has_decimals(roll.result)}"
     )
+
+
+def _bonus_strings_from_bonus(bonus: float) -> tuple[str, str]:
+    """Return:
+    (request bonus, after rolls bonus)
+    Request bonus: number with sign
+                   e.g. "+3", "-1"
+                   or empty string if zero
+    After rolls bonus: number with sign and spaces before and after the sign
+                       e.g. " + 3", " - 1"
+                       or empty string if zero
+    Numbers should be formatted as integers unless they have decimals
+    In that case they should be formatted as floats"""
+    if bonus:
+        bonus = as_float_if_has_decimals(bonus)
+        request_bonus = number_as_string_with_sign(bonus)
+        after_rolls_bonus = number_as_string_with_spaces_around_sign(bonus)
+    else:
+        request_bonus = ""
+        after_rolls_bonus = ""
+    return request_bonus, after_rolls_bonus
 
 
 def as_float_if_has_decimals(number: Union[int, float]) -> Union[int, float]:
